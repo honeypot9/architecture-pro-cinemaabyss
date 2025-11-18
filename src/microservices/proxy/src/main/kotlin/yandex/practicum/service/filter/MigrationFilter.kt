bc44907acad6ccdf61d475010d14878a6752d993
Denis.Marunich <Denis.Marunich@rt.ru>
2025-11-18 06:00:03 +0300

    ARCH_SPRINT_2: Задание 3 приведе адресов сервисов к именам контейнеров в Docker сети

--- src/microservices/proxy/src/main/kotlin/yandex/practicum/service/filter/MigrationFilter.kt
+++ src/microservices/proxy/src/main/kotlin/yandex/practicum/service/filter/MigrationFilter.kt
@@ -22,8 +22,8 @@ class MigrationFilter(
 
     private val config = MigrationConfig(
         migrationPercent = applicationProperties.migrationPercent?: 50,
-        oldService = "http://localhost:8080",
-        newService = "http://localhost:8081"
+        oldService = "http://movies-service:8080",
+        newService = "http://monolith:8081"
     )
 
     override fun filter(exchange: ServerWebExchange, chain: GatewayFilterChain): Mono<Void> {
